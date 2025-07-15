4f06f567e6494cf3895e63268a469b25ba1427f2
oredytech <sonybabaoredy@gmail.com>
2025-07-15 22:36:08 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,6 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
-        "react-helmet-async": path.resolve(__dirname, "node_modules/react-helmet-async"),
       },
     },
     build: {
