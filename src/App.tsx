8d3b8dd011fefb117bf7314c70161fedf9890e4f
oredytech <sonybabaoredy@gmail.com>
2025-07-15 22:07:01 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HelmetProvider } from "react-helmet-async";
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 import { Toaster } from "@/components/ui/toaster";
 import Index from "./pages/Index";
 import About from "./pages/About";
