f13418aea39aa01cf8204ad9679c8cffb07a541c
oredytech <sonybabaoredy@gmail.com>
2025-07-15 23:05:03 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { TranslationProvider } from './hooks/useTranslations'; // ← adapte le chemin si nécessaire
+import { TranslationProvider } from './hooks/useTranslation'; // ← adapte le chemin si nécessaire
 import './index.css';
 
 createRoot(document.getElementById("root")!).render(
